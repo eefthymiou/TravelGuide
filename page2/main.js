62289d853a353829938ec94a1b7f8b0e1a80e067
eefthymiou <johnefthimiou1002@gmail.com>
2023-03-26 00:49:21 +0200

    reviews added

--- page2/main.js
+++ page2/main.js
@@ -23,31 +23,128 @@ function disableTextarea() {
 }
 
 
-function previewPhoto() {
-    let fileInput = document.getElementById("inputGroupFile");
-    let previewButton = document.getElementById("previewButton");
-    let previewImage = document.getElementById("previewImage");
+previewButton.addEventListener("click", function() {
+    const fileInput = document.getElementById("inputGroupFile");
+    const divImages = document.getElementById("imagesContainer");
+    const previewImage = document.createElement("img");
+    previewImage.className = "img-fluid";
 
-    previewButton.addEventListener("click", function() {
-        let fileInput = document.getElementById("inputGroupFile");
-        let previewButton = document.getElementById("previewButton");
-        let previewImage = document.getElementById("previewImage");
+    // Prevent the default action
+    event.preventDefault();
 
-        let file = fileInput.files[0];
-        let fileType = file.type.toLowerCase();
-        if (fileType.indexOf("image/") == 0) {
-            let reader = new FileReader();
-            reader.onload = function(e) {
-                previewImage.src = e.target.result;
-                previewImage.style.display = "block";
-            };
-            reader.readAsDataURL(file);
-        } else {
-            alert("Please select an image file.");
-          }
-    });
+    let file = fileInput.files[0];
+    let fileType = file.type.toLowerCase();
+    if (fileType.indexOf("image/") == 0) {
+        let reader = new FileReader();
+        reader.onload = function(e) {
+            previewImage.src = e.target.result;
+        };
+        reader.readAsDataURL(file);
+    } else {
+        alert("Please select an image file.");
+    }
 
-}
+    // add image to div
+    divImages.appendChild(previewImage);
+});
+
+
+addReview.addEventListener("click", function() {
+    const container = document.getElementById("reviewContainer");
+    // Prevent the default action
+    event.preventDefault();
+
+    // Check if user has already written a review
+
+    // Create a new div element
+    const newDiv = document.createElement("div");
+    newDiv.className = "card mb-3 shadow p-3";
+
+    // create div for row
+    const newRow = document.createElement("div");
+    newRow.className = "row g-0";
+
+    // create div for col-md-3
+    const newCol1 = document.createElement("div");
+    newCol1.className = "col-md-3";
+
+    // create div for col-md-3 d-flex align-items-center h-100
+    const newCol2 = document.createElement("div");
+    newCol2.className = "d-flex align-items-center h-100";
+
+    // create p element for user name
+    const newP = document.createElement("p");
+    newP.className = "card-text md-3 mx-auto";
+    newP.innerText = "Όνομα Χρήστη";
+
+    // create div for col-md-8
+    const newCol3 = document.createElement("div");
+    newCol3.className = "col-md-8";
+
+    // create div for card-body
+    const newCardBody = document.createElement("div");
+    newCardBody.className = "card-body";
+
+    // create h5 element for rating
+    const newH5 = document.createElement("h5");
+    newH5.className = "card-title";
+    newH5.innerText = "Βαθμολογία";
+
+    // create p element for review
+    const newP2 = document.createElement("p");
+    newP2.className = "card-text";
+    newP2.innerText = "Η παραλία αυτή είναι εξαιρετική.";
+
+    // create p element for date
+    const newP3 = document.createElement("p");
+    newP3.className = "card-text";
+
+    // create small element for date
+    const newSmall = document.createElement("small");
+    newSmall.className = "text-body-secondary";
+    newSmall.innerText = "Ημερομηνία Δημιοσίευσης 28/02/2020";
+
+    // append elements
+    newP3.appendChild(newSmall);
+    newCol2.appendChild(newP);
+    newCol1.appendChild(newCol2);
+    newCardBody.appendChild(newH5);
+    newCardBody.appendChild(newP2);
+    newCardBody.appendChild(newP3);
+    newCol3.appendChild(newCardBody);
+    newRow.appendChild(newCol1);
+    newRow.appendChild(newCol3);
+    newDiv.appendChild(newRow);
+    container.appendChild(newDiv);
+
+    // Create a new div element
+    const newRowButtons = document.createElement("div");
+    newRowButtons.className = "gap-2 d-flex justify-content-start";
+
+    // Create a new button element
+    const newSaveButton = document.createElement("button");
+    newSaveButton.className = "btn btn-success mb-3";
+    newSaveButton.innerText = "Αποθήκευση";
+
+    // Create a new button element
+    const newEditButton = document.createElement("button");
+    newEditButton.className = "btn btn-warning mb-3";
+    newEditButton.innerText = "Επεξεργασία";
+
+    // Create a new button element
+    const newDeleteButton = document.createElement("button");
+    newDeleteButton.className = "btn btn-danger mb-3";
+    newDeleteButton.innerText = "Διαγραφή";
+
+    // Append the button to the div
+    newRowButtons.appendChild(newSaveButton);
+    newRowButtons.appendChild(newEditButton);
+    newRowButtons.appendChild(newDeleteButton);
+
+    // Append the div to the container
+    container.appendChild(newRowButtons);
+});
+
 
 addButton.addEventListener("click", function() {
     const container = document.getElementById("container");
@@ -132,10 +229,19 @@ function editButtonClicked(callback) {
     });
 }
 
+function hideUserAction(){
+    let elements = document.getElementsByClassName("userInput");
+    for (let i = 0; i < elements.length; i++){
+        elements[i].style.display = "none";
+    }
+}
+
 function admin_page() {
     disableTextarea()
     hideAdminAction()
-    edit = false
+    hideUserAction()
+
+    edit = false;
 
     editButtonClicked(function(edit) {
         if (!edit){
@@ -144,7 +250,6 @@ function admin_page() {
         }
         else {
             enableTextarea()
-            previewPhoto()
             showAdminAction()
         }
     });
