829102447b7cf2f1f376971269b612a0150d22a5
marosimosi <marosimosi@gmail.com>
2023-03-31 20:16:08 +0300

    link page1 to page2

--- page1/page1.js
+++ page1/page1.js
@@ -1,14 +1,13 @@
 // PAGE1 NAVBAR ---> PAGE1
-const pori = {"name":"Πορί", "description":"Η παραλία Πορί έχει μια απίστευτη ομορφιά που καθηλώνει τον επισκέπτη. Αποτελείται από λευκή άμμο με σμαραγδένια νερά και είναι τεράστια σε μήκος. Θεωρείται μια από τις κορυφαίες παραλίες των Κυκλάδων",
+const pori = {"id": 1, "name":"Πορί", "description":"Η παραλία Πορί έχει μια απίστευτη ομορφιά που καθηλώνει τον επισκέπτη. Αποτελείται από λευκή άμμο με σμαραγδένια νερά και είναι τεράστια σε μήκος. Θεωρείται μια από τις κορυφαίες παραλίες των Κυκλάδων",
 "image": "../images/pori.png"};
 
-const italida = {"name":"Ιταλίδα", "description":"Το όνομα “Ιταλίδα”, προέκυψε λόγω της ιδιοκτήτριας της περιοχής πάνω από την παραλία. Αλλιώς, η Πλατιά Πούντα που είναι και το πραγματικό της όνομα είναι μία από τις πολυσύχναστες παραλίες του νησιού αμμώδης με γαλαζοπράσινα, κρυστάλλινα νερά.Απέχει 1.800 μέτρα περίπου από το χωριό και φτάνετε με καραβάκι ή με πεζοπορία 30 περίπου λεπτών.",
+const italida = {"id": 2,"name":"Ιταλίδα", "description":"Το όνομα “Ιταλίδα”, προέκυψε λόγω της ιδιοκτήτριας της περιοχής πάνω από την παραλία. Αλλιώς, η Πλατιά Πούντα που είναι και το πραγματικό της όνομα είναι μία από τις πολυσύχναστες παραλίες του νησιού αμμώδης με γαλαζοπράσινα, κρυστάλλινα νερά.Απέχει 1.800 μέτρα περίπου από το χωριό και φτάνετε με καραβάκι ή με πεζοπορία 30 περίπου λεπτών.",
 "image": "../images/italida.jpg"};
 
 const beaches = [pori, italida, pori, italida];
 
 const title = document.querySelector("p.title");
-title.textContent = localStorage.getItem("selectedOption");
 updatePage();
 
 const dropdown = document.querySelector(".dropdown-menu");
@@ -44,13 +43,15 @@ function createCard(item){
   const row = document.querySelector("div.row");
   const column = document.createElement("div");
   column.classList.add("col");
-  const card = document.createElement("div");
+  const card = document.createElement("a");
   
   card.classList.add("card");
   //card.setAttribute("data-value", "name");
   //card.style.width = "20px";
   // card.style.height = "300px";
   card.style.marginInline= "30px";
+  card.href = "../page2/page2.html";
+  card.datavalue = item.id;
   const cardImage = document.createElement("img");
   cardImage.classList.add("card-img-top");
   // cardImage.style.width = "250px-lg";
@@ -70,3 +71,11 @@ function createCard(item){
   column.appendChild(card);
   row.appendChild(column);
 }
+
+const cards = document.querySelectorAll('.card');
+cards.forEach(card => {
+  card.addEventListener('click', () => {
+    selectedPlace = card.getAttribute('data-value');
+    localStorage.setItem("selectedPlace", selectedPlace);
+  });
+});
