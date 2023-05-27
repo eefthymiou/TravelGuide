e615e37ae33983dedfceda134459fae9177bf614
eefthymiou <johnefthimiou1002@gmail.com>
2023-05-27 20:20:42 +0300

    try catch to mongoose.connect

--- model/mongodb/mongodb.mjs
+++ model/mongodb/mongodb.mjs
@@ -16,17 +16,23 @@ try {
     throw Error('Δεν ήταν δυνατό να ανοίξει η βάση δεδομένων.' + error);
 }
 
-// assigns the connection object returned by mongoose.connection to the db constant
-// the db object represents the connection to the MongoDB database.
-const db = mongoose.connection;
+try {
+    // assigns the connection object returned by mongoose.connection to the db constant
+    // the db object represents the connection to the MongoDB database.
+    const db = mongoose.connection;
 
-// db.on sets up an event listener for the 'error' event on the database connection
-db.on('error', console.error.bind(console, 'connection error:'));
+    // db.on sets up an event listener for the 'error' event on the database connection
+    db.on('error', console.error.bind(console, 'connection error:'));
 
-// db.once event listener for the 'open' event on the database connection
-db.once('open', function () {
-    console.log("Successfull connection");
-});
+    // db.once event listener for the 'open' event on the database connection
+    db.once('open', function () {
+        console.log("Successfull connection");
+    });
+}
+catch (error) {
+    throw Error('Δεν ήταν δυνατό να ανοίξει η βάση δεδομένων.' + error);
+}
+
 
 
 
